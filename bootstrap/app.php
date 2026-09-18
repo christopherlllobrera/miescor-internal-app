9cd82d0889bf4b7bbc104ead8c8b40f80db8db07
Christopher Llobrera <163362646+christopherlllobrera@users.noreply.github.com>
2026-09-19 06:23:58 +0800

    added security headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->append(SecurityHeaders::class);
         $middleware->redirectGuestsTo(fn (Request $request) => route('filament.services.auth.login'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
